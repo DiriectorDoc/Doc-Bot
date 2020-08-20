186f909e694ac4d5ad39bab349f6d054a2108692
DiriectorDoc <66105586+DiriectorDoc@users.noreply.github.com>
2020-08-20 17:37:19 -0400

    Continuing development

--- index.js
+++ index.js
@@ -1,13 +1,34 @@
 const Discord = require("discord.js"),
 	  bot = new Discord.Client(),
-	  token = /* Not included for security reasons */,
-	  modIDs = [/* Not included for security reasons */];
+	  config = require('./config.json');
+
+let self;
 
 function modOnly(msg, call){
-	if(modIDs.some(id => id === msg.author.id)){
+	if(config.modIDs.some(id => id === msg.author.id)){
 		call()
 	} else {
-		msg.reply("Excuse me. Are you an admin? No? I didn't think so. Lay of the admin commands, m'kay.")
+		msg.reply(
+			[
+				"Excuse me? ",
+				"Ah, ah, ah. ",
+				"What are you doing? ",
+				"Stop right there! ",
+				"Um...",
+				"Um, no. "
+			][Math.round(Math.random()*6)] +
+			[
+				"Are you an admin? No? I didn't think so. ",
+				"You need to be an admin to use that command. ",
+				"Only admins can do that. ",
+				"You tried, but that's an admin-only command. "
+			][Math.round(Math.random()*4)] +
+			[
+				"Lay of the admin commands, m'kay?",
+				"Try a different command, you might have some luck.",
+				"Use *!commands* to see the availible commands"
+			][Math.round(Math.random()*3)]
+		)
 	}
 }
 
@@ -17,37 +38,94 @@ function compileDM(msg){
 
 bot.on("ready", function(){
 	console.log("Doc Bot is online")
+	bot.users.fetch(config.botID, false).then(bot => {
+		self = bot;
+	})
 })
 
 bot.on("message", function(msg){
 	if(msg.author.bot)
 		return;
-	if(msg.channel.name == "talk-to-the-doc"){
-		if(msg.author.id != modIDs[0]){
-			bot.users.fetch(modIDs[0], false).then(user => {
-				user.send(compileDM(msg))
+	switch(msg.channel.name){
+		case "talk-to-the-doc":
+			if(msg.author.id != config.modIDs[0]){
+				bot.users.fetch(config.modIDs[0], false).then(user => {
+					user.send(compileDM(msg))
+				})
+			}
+			return;
+		case "promotion":
+			bot.channels.fetch(config.archiveID).then(channel => {
+				channel.send(new Discord.MessageEmbed({
+					title: "Archived promotion",
+					color: msg.member.displayHexColor,
+					author: {
+						name: msg.author.username + "#" + msg.author.discriminator,
+						icon_url: msg.author.displayAvatarURL()
+					},
+					description: msg.content,
+					footer: {
+						text: new Date(msg.createdTimestamp).toUTCString().replace("GMT", "UTC")
+					}
+				}))
 			})
-		console.log(msg)
-		}
-		return
+			msg.delete({
+				timeout: 86400000,
+				reason: "Automatic. Promotions are deleted after 24 hours."
+			})
+			return;
+		default:
+			if(msg.content.match(/^!.+/g)){
+				let args = msg.content.split(" ");
+				switch(args.shift().substring(1)){
+					case "commands":
+						msg.reply(new Discord.MessageEmbed({
+							title: "Commands",
+							color: 0x3498DB,
+							author: {
+								name: "Doc Bot",
+								icon_url: self.displayAvatarURL()
+							},
+							description: "list of commands",
+							fields: [
+								{
+									name: "Commands",
+									value: "`!commands`\n`!color`",
+									inline: true
+								},
+								{
+									name: "Purpose",
+									value: "Displays this here list\nChanges username color",
+									inline: true
+								}
+							],
+							footer: {
+								text: new Date().toUTCString().replace("GMT", "UTC")
+							}
+						}))
+						break;
+					case "color":
+					case "colour":
+						msg.guild.members.fetch(msg.author.id).then(guildMember =>{
+							guildMember.roles.set(config.colourIDs[args[0].toLowerCase()] || [])
+						})
+						break;
+					case "notify":
+						modOnly(msg, () => {
+							msg.channel.send("@everyone " + args.join(" "))
+						})
+						break;
+					case "stop":
+						modOnly(msg, () => {
+							throw "Doc Bot has been sucessfully stopped."
+						})
+						break;
+					default:
+						msg.reply("What were you thinking? That's not a command.")
+				}
+			}
 	}
-	if(msg.content.match(/^!.+/g)){
-		let args = msg.content.split(" ");
-		switch(args.shift().substring(1)){
-			case "notify":
-				modOnly(msg, () => {
-					msg.channel.send("@everyone " + args.join(" "))
-				})
-				break;
-			case "stop":
-				modOnly(msg, () => {
-					throw "Doc Bot has been sucessfully stopped."
-				})
-				break;
-			default:
-				msg.reply("What were you thinking? That's not a command.")
-		}
-	}
+			
 })
 
-bot.login(token)
+bot.login(config.token)

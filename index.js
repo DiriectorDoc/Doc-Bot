bc65039abbf15da6cb7f24232c61eb4a9a7de1af
Diriector_Doc <davijust213@yahoo.ca>
2021-01-08 13:44:37 -0500

    Updated version number

--- index.js
+++ index.js
@@ -241,7 +241,7 @@ bot.on("message", function(msg){
 							fields: [
 								{
 									name: "Version",
-									value: "0.8.0",
+									value: "0.9.0",
 									inline: true
 								},
 								{
